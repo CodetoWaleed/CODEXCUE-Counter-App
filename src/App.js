4cbfb701b6b05c9358ecc8bd7c99b08b68fe057a
Waleed Ahmad <waleedahmad968@gmail.com>
2024-05-05 02:11:30 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './CounterApp.css'; // Import CSS file for styling
 
 function App() {
+  // State variable for the counter
+  const [count, setCount] = useState(0);
+
+  // Function to handle incrementing the counter
+  const increment = () => {
+
+    setCount(count  + 1);
+  };
+
+  // Function to handle decrementing the counter
+  const decrement = () => {
+    setCount(count  - 1);
+  };
+
+  // Function to handle resetting the counter
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="counter-container">
+      <h1 className="title">CODEXCUE Counter App</h1>
+      <div className="counter">
+      <p className="count"><span class="count-number">Count: {count}</span></p>
+        <div className="button-container">
+        <button className="btn increment" onClick={increment}>Increment</button>
+        <button className="btn decrement" onClick={decrement}>Decrement</button>
+<button className="btn reset" onClick={reset}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
